de26232b507bdafd1b2bfec31623a3e86ca62874
alaminv2 <alaminversion2@gmail.com>
2022-01-18 18:30:46 +0600

    naknd

--- tree.py
+++ tree.py
@@ -1,6 +1,7 @@
 from collections import deque
 
 
+# General tree
 class TreeNode:
     def __init__(self, name=None, designation=None, parent=None):
         self.name = name
@@ -35,6 +36,7 @@ class TreeNode:
         return f'{self.name}, {self.designation}'
 
 
+# Binary tree
 class BinaryTree:
     def __init__(self, data):
         self.data = data
@@ -49,7 +51,6 @@ class BinaryTree:
         queue = deque()
         result = list()
         queue.append(self)
-        print(queue)
         while queue:
             cur = queue.popleft()
             if cur.left:
@@ -59,7 +60,39 @@ class BinaryTree:
             result.append(cur.data)
         return result
 
-# Level order tree traversal
+    def insert_level_order(self, data):
+        queue = deque()
+        queue.append(self)
+        while queue:
+            cur = queue.popleft()
+            if not cur.left:
+                cur.left = BinaryTree(data)
+                break
+            else:
+                queue.append(cur.left)
+
+            if not cur.right:
+                cur.right = BinaryTree(data)
+                break
+            else:
+                queue.append(cur.right)
+
+        # ---Method using level
+        # ---get the height of tree
+        # ---iterate over height and print all nodes of that level
+        # height = self.get_tree_height()
+        # for i in range(1, height+1):
+        #     self.print_current_level(i)
+
+    def delete_node(self, key):
+        if not self:
+            return
+        queue = list()
+        queue.append(self)
+        while queue:
+            pass
+
+    # Level order tree traversal
     def get_tree_height(self):
         l_height = 0 if not self.left else self.left.get_tree_height()
         r_height = 0 if not self.right else self.right.get_tree_height()
@@ -69,29 +102,20 @@ class BinaryTree:
         else:
             return r_height + 1
 
-    def get_current_level(self, level):
-        pass
-
     def __str__(self):
         return str(self.data)
 
 
 if __name__ == '__main__':
-    parent = BinaryTree(20)
-    parent.left = BinaryTree(15)
-    parent.right = BinaryTree(22)
-    parent.left.left = BinaryTree(10)
-    parent.right.left = BinaryTree(16)
-    parent.right.right = BinaryTree(19)
-    # parent.add_child(30)
-    # parent.add_child(12)
-    # parent.add_child(13)
-    # parent.add_child(5)
-    # parent.add_child(21)
-    # parent.add_child(25)
-    # parent.add_child(23)
-    # parent.add_child(24)
+    parent = BinaryTree(10)
+    parent.left = BinaryTree(11)
+    parent.right = BinaryTree(9)
+    parent.left.left = BinaryTree(7)
+    parent.right.left = BinaryTree(15)
+    parent.right.right = BinaryTree(8)
 
-    print('height = ', parent.get_tree_height())
-    res = parent.level_order_print()
-    print(res)
+    parent.insert_level_order(12)
+    print(parent.level_order_print())
+
+    parent.delete_node(12)
+
